7d9755d50abcf454f9084151398a2d0323ede20b
kuranuki <kuranuki@sonicgarden.jp>
2017-12-01 14:07:35 +0900

    iframe対応しようとしてる（がうまくいかなかった）

--- app/src/main/java/jp/sonicgarden/socialchange/BlogPostActivity.kt
+++ app/src/main/java/jp/sonicgarden/socialchange/BlogPostActivity.kt
@@ -5,12 +5,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_blog_post.*
 
 class BlogPostActivity : AppCompatActivity() {
 
     private lateinit var realm: Realm
+    private lateinit var blogPostModel: BlogPostModel
 
     companion object {
         fun newIntent(context: Context, postId: String): Intent {
@@ -20,21 +23,26 @@ class BlogPostActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blog_post)
 
         realm = Realm.getDefaultInstance()
         val postId = intent.getStringExtra("postId")
-        val blogPostModel = BlogPostModel.find(realm, postId)
-
-        webView.settings.javaScriptEnabled = true
-        webView.loadDataWithBaseURL(null, blogPostModel?.content, "text/html", "UTF8", null)
+        blogPostModel = BlogPostModel.find(realm, postId) ?: BlogPostModel()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         realm.close()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onResume() {
+        super.onResume()
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadDataWithBaseURL(null, blogPostModel.content, "text/html", "UTF8", null)
     }
 }
